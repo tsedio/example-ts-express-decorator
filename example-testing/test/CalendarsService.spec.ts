2630d3965911ec682ee0783eb644a96add0786b1
Romain Lenzotti <rom.lenzotti@gmail.com>
2016-11-26 10:13:00 +0100

    Add test example

--- example-testing/test/CalendarsService.spec.ts
+++ example-testing/test/CalendarsService.spec.ts
@@ -1,1 +1,17 @@
+
 import {inject} from "ts-express-decorators/testing";
+import {expect} from "chai";
+import CalendarsService from '../../example-testing/services/CalendarsService';
+
+describe('CalendarsService:', () => {
+
+    it('should do inject service', inject([CalendarsService],
+        (calendarsService: CalendarsService) => {
+
+
+            expect(calendarsService).to.be.an.instanceof(CalendarsService);
+
+
+        }
+    ));
+});

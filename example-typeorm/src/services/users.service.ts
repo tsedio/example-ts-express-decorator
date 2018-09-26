c6374ef91da284e404453e2801b1739793d3a3d5
romain.lenzotti <rom.lenzotti@gmail.com>
2018-09-26 19:29:26 +0200

    fix: Update dependency

--- example-typeorm/src/services/users.service.ts
+++ example-typeorm/src/services/users.service.ts
@@ -1,51 +1,51 @@
 import {IComponentScanned, Service} from "@tsed/common";
 import {TypeORMService} from "@tsed/typeorm";
+import {$log} from "ts-log-debug";
 import {Connection} from "typeorm";
 import {User} from "../entity/user";
-import {$log} from "ts-log-debug";
 
 @Service()
 export class UsersService {
-    private connection: Connection;
+  private connection: Connection;
 
-    constructor(private typeORMService: TypeORMService) {
-    }
+  constructor(private typeORMService: TypeORMService) {
+  }
 
-    $onInit(): Promise<any> | void {
-        $log.warn('All services is ready');
-    }
+  $onInit(): Promise<any> | void {
+    $log.warn("All services is ready");
+  }
 
-    $beforeRoutesInit(): Promise<any> | void {
-        $log.warn('Controllers and routes isn\'t mounted');
-    }
+  $beforeRoutesInit(): Promise<any> | void {
+    $log.warn("Controllers and routes isn't mounted");
+  }
 
-    $onRoutesInit(components: IComponentScanned[]): Promise<any> | void {
-        $log.warn('Controllers and routes are being built');
+  $onRoutesInit(components: IComponentScanned[]): Promise<any> | void {
+    $log.warn("Controllers and routes are being built");
 
-    }
+  }
 
-    $afterRoutesInit(): Promise<any> | void {
-        $log.warn('Controllers and routes are built');
-        this.connection = this.typeORMService.get();
-        console.log("this.connection =>", !!this.connection);
-    }
+  $afterRoutesInit(): Promise<any> | void {
+    $log.warn("Controllers and routes are built");
+    this.connection = this.typeORMService.get();
+    console.log("this.connection =>", !!this.connection);
+  }
 
-    $onServerReady(): Promise<any> | void {
-        $log.warn('Server is ready and listen the port');
-    }
+  $onServerReady(): Promise<any> | void {
+    $log.warn("Server is ready and listen the port");
+  }
 
-    async create(user: User): Promise<User> {
-        await this.connection.manager.save(user);
-        console.log("Saved a new user with id: " + user.id);
+  async create(user: User): Promise<User> {
+    await this.connection.manager.save(user);
+    console.log("Saved a new user with id: " + user.id);
 
-        return user;
-    }
+    return user;
+  }
 
-    async find(): Promise<User[]> {
-        const users = await this.connection.manager.find(User);
-        console.log("Loaded users: ", users);
+  async find(): Promise<User[]> {
+    const users = await this.connection.manager.find(User);
+    console.log("Loaded users: ", users);
 
-        return users;
-    }
+    return users;
+  }
 
 }
